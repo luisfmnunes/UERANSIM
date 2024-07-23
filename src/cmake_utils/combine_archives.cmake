78c9468bca15c96cf8233a7d9db49d002d47f091
Luís Felipe Oliveira de Melo <luisfelipe5417@gmail.com>
2024-07-23 16:45:48 +0200

    Fixed combined archive linking to onppgnb interface

--- src/cmake_utils/combine_archives.cmake
+++ src/cmake_utils/combine_archives.cmake
@@ -22,10 +22,19 @@ function(combine_archives output_archive)
 
     add_library(${output_archive} STATIC ${output_archive_dummy_file})
 
+    target_compile_options(${output_archive} PUBLIC -Wall -Wextra -pedantic -Wno-unused-parameter)
+
     # Add a custom command to combine the archives after the static library is "built".
     add_custom_command(TARGET ${output_archive}
             POST_BUILD
             COMMAND ar -M < ${mri_file}
             COMMENT "Combining static libraries for ${output_archive}"
             )
+    FOREACH(in_target ${ARGN})
+        target_link_libraries(${output_archive} PUBLIC ${in_target})
+    ENDFOREACH()
+
+    # Clean up the dummy file after the combined library is built.
+    file(REMOVE ${CMAKE_BINARY_DIR}/${output_archive}.dummy.cpp)
+
 endfunction(combine_archives)
